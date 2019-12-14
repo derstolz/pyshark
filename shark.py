014e786a5df43ef245106ad386c5b30cb93fd682
retr0 <downtownxore@gmail.com>
2019-12-14 14:24:28 -0500

    working on shark

--- shark.py
+++ shark.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
-import logging
 
 import pyshark
-
-logging.basicConfig(format='[%(asctime)s %(levelname)s]: %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p',
-                    level='INFO')
 
 DEFAULT_OUTPUT_FILE = 'shark-output.txt'
 
@@ -20,6 +15,11 @@ def get_arguments():
                         dest='interface',
                         required=True,
                         help='Specify a network interface to listen and dump the network traffic.')
+    parser.add_argument('--dns',
+                        action='store_true',
+                        default=False,
+                        required=False,
+                        help='Pass this argument to enable dns sniffing. Default is False')
     parser.add_argument('-o',
                         '--output',
                         dest='output',
@@ -35,44 +35,31 @@ options = get_arguments()
 
 
 class Shark:
-    def __init__(self, network_interface, output_file):
+    def __init__(self, network_interface, output_file, capture_dns=False):
         self.network_interface = network_interface
         self.output_file = output_file
+        self.capture_dns = capture_dns
 
     def start(self):
         capture = pyshark.LiveCapture(interface=options.interface)
         capture.set_debug()
-        capture.apply_on_packets(self.catch_dns_request)
+        capture.apply_on_packets(self.sniff_packet)
 
-    def catch_dns_request(self, packet):
-        # https://serverfault.com/questions/173187/what-does-a-dns-request-look-like
-        try:
-            layer_dns = packet.dns
-        except AttributeError:
-            return
-        try:
-            resp = layer_dns.resp
-            name = resp.name
-            dns_type = resp.type
-            packet_type = 'RESPONSE'
-        except AttributeError:
-            name = layer_dns.qry_name
-            dns_type = layer_dns.qry_type
-            packet_type = 'REQUEST'
-
-        try:
-            ip = getattr(packet, "ip", packet.ipv6)
-            ip_route = f"{ip.src} -> {ip.dst}"
-        except AttributeError:
-            ip_route = "unknown"
-
-        with open(self.output_file, 'a', encoding='utf-8') as f:
-            f.write(f"{packet.sniff_time:%d-%m-%Y %H:%M:%S} DNS {dns_type} {packet_type} {name} {ip_route}\n")
+    def sniff_packet(self, packet):
+        log_message = f'{packet.transport_layer} {packet.highest_layer} {packet.ip.src} -> {packet.ip.dst}'
+        if self.capture_dns:
+            if hasattr(packet, 'dns'):
+                dns = packet.dns
+                if hasattr(dns, 'qry_name'):
+                    if hasattr(dns, 'a'):
+                        print(f'{log_message} {dns.qry_name} {dns.a}')
+                    else:
+                        print(f'{log_message} {dns.qry_name}')
 
 
 try:
-    shark = Shark(network_interface=options.interface, output_file=options.output)
+    shark = Shark(network_interface=options.interface, output_file=options.output, capture_dns=options.dns)
     shark.start()
 except KeyboardInterrupt:
-    logging.warning('\nInterrupted')
+    print('\nInterrupted')
     exit(1)

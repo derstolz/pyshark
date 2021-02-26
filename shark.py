8b8e2be7820ac9a2d9405cc28c5dad43f44840d8
Kali Linux <kali@dionysus.kali.org>
2021-02-26 12:40:44 +0000

    add read files features

--- shark.py
+++ shark.py
@@ -13,8 +13,18 @@ def get_arguments():
     parser.add_argument('-i',
                         '--interface',
                         dest='interface',
-                        required=True,
+                        required=False,
                         help='Specify a network interface to listen and dump the network traffic.')
+    parser.add_argument('-r',
+                        '--read',
+                        dest='read',
+                        required=False,
+                        help='Specify a network dump file to read and extract the IP addresses.')
+    parser.add_argument('-f',
+                        '--filter',
+                        dest='filter',
+                        required=False,
+                        help='Specify a custom filter to apply for your packets.')
     parser.add_argument('--dns',
                         action='store_true',
                         default=False,
@@ -33,13 +43,60 @@ def get_arguments():
                         help='Specify an output file to write captured data. Default is ' + DEFAULT_OUTPUT_FILE)
     options = parser.parse_args()
 
+    if (not options.read and not options.interface) or (options.read and options.interface):
+        parser.error('You have to specify either the network interface or the network dump file')
     return options
 
 
 options = get_arguments()
 
+class FileShark:
+    def __init__(self, pcapng_file,
+                 output_file,
+                 filter=None):
+        self.pcapng_file = pcapng_file
+        self.output_file = output_file
+        self.filter = filter
 
-class Shark:
+    def extract_ips(self):
+        if not self.filter:
+            cap = pyshark.FileCapture(self.pcapng_file)
+        else:
+            cap = pyshark.FileCapture(self.pcapng_file, display_filter=options.filter)
+        for packet in cap:
+
+            protocol = packet.highest_layer
+            message = f"[{protocol}] "
+            if 'ip' in packet:
+                ip_src = packet['ip'].src
+                ip_dest = packet['ip'].dst
+
+                if 'tcp' in packet:
+                    src_port = packet.tcp.srcport
+                    dest_port = packet.tcp.dstport
+                elif 'udp' in packet:
+                    src_port = packet.udp.srcport
+                    dest_port = packet.udp.dstport
+                else:
+                    src_port = ''
+                    dest_port = ''
+
+                if src_port and dest_port:
+                    message += f"{ip_src}:{src_port} -> {ip_dest}:{dest_port}"
+                else:
+                    message += f"{ip_src} -> {ip_dest}"
+            elif 'eth' in packet:
+                mac_src = packet['eth'].src
+                mac_dest = packet['eth'].dst
+                message += f"{mac_src} -> {mac_dest}"
+
+            print(message)
+
+    def apply_filter(self):
+        pass
+
+
+class LiveShark:
     def __init__(self, network_interface,
                  output_file,
                  capture_dns=False,
@@ -97,15 +154,25 @@ class Shark:
                       f'{host} {cookie} {authorization} {user_agent}')
 
 
-try:
-    shark = Shark(network_interface=options.interface,
-                  output_file=options.output,
-                  capture_dns=options.dns,
-                  capture_http=options.http)
-    shark.start()
-except KeyboardInterrupt:
-    print('\nInterrupted')
-    exit(0)
-except Exception as e:
-    print(f'Unexpected error: {e}')
-    exit(1)
+if options.interface:
+    try:
+        shark = LiveShark(network_interface=options.interface,
+                          output_file=options.output,
+                          capture_dns=options.dns,
+                          capture_http=options.http)
+        shark.start()
+    except KeyboardInterrupt:
+        print('\nInterrupted')
+        exit(0)
+    except Exception as e:
+        print(f'Unexpected error: {type(e)} - {e}')
+        exit(1)
+if options.read:
+    try:
+        file_shark = FileShark(pcapng_file=options.read,
+                               output_file=options.output,
+                               filter=options.filter)
+        file_shark.extract_ips()
+    except Exception as e:
+        print(f"Unexpected error: {type(e)} - {e}")
+        exit(1)
